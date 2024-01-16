1e4dfbc77b3210f4470264e6c81da103684dbd4b
OrhunYildirimOgut <o.ogut@etu.edu.tr>
2024-01-17 02:25:03 +0300

    First commit

---
+++ srp/Program.cs
@@ -0,0 +1,75 @@
+﻿/*public class Kus
+{
+    Boolean amIFly;
+    int fleshCount;
+    public Kus() { }
+
+    public void fly()
+    {
+        amIFly = true;
+    }
+
+    public void land()
+    {
+        amIFly = false;
+    }
+
+    public void eat(int fleshNumber)
+    {
+        fleshCount+= fleshNumber;
+        
+    }
+
+    public void printflesh()
+    {
+        Console.WriteLine(fleshCount);
+    }
+
+    public void printAmIFly()
+    {
+        Console.WriteLine(amIFly);
+    }
+
+
+}*/
+
+public class Kus
+{
+    public Boolean amIFly;
+    public int fleshCount;
+    public Kus() { }
+
+    public void fly()
+    {
+        amIFly = true;
+    }
+
+    public void land()
+    {
+        amIFly = false;
+    }
+
+    public void eat(int fleshNumber)
+    {
+        fleshCount += fleshNumber;
+    }
+}
+
+public class KusPrint
+{
+    Kus holder;
+    public KusPrint(Kus kus)
+    {
+        holder = kus;
+    }
+
+    public void printAmIFly()
+    {
+        Console.WriteLine(holder.amIFly);
+    }
+
+    public void printflesh()
+    {
+        Console.WriteLine(holder.fleshCount);
+    }
+}
